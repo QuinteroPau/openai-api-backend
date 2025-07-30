ab20025a2190b948e46b00334f44f86ad59e04fb
QuinteroPau <pauqb02@gmail.com>
2025-07-30 19:55:41 +0200

    a

--- api/chat.ts
+++ api/chat.ts
@@ -1,5 +1,6 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).json({ message: 'Chat endpoint is working!' });
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  res.status(200).json({ message: 'Chat API is alive!' });
 }
